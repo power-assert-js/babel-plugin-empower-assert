65abb7e7b15fba182a5fd1992aebfe662aa5702f
Takuto Wada <takuto.wada@gmail.com>
2018-03-30 02:09:28 +0900

    test: dealing with CJS/ESM interop

--- test/test.js
+++ test/test.js
@@ -8,16 +8,15 @@ var path = require('path');
 var babel = require('babel-core');
 var extend = require('xtend');
 
-function testTransform (fixtureName, extraOptions, extraSuffix) {
+function testTransform (fixtureName, extension) {
     it(fixtureName, function () {
-        var suffix = extraSuffix ? '-' + extraSuffix : '';
-        var fixtureFilepath = path.resolve(__dirname, 'fixtures', fixtureName, 'fixture.js');
-        var expectedFilepath = path.resolve(__dirname, 'fixtures', fixtureName, 'expected' + suffix + '.js');
-        var result = babel.transformFileSync(fixtureFilepath, extend({
+        var fixtureFilepath = path.resolve(__dirname, 'fixtures', fixtureName, 'fixture.' + extension);
+        var expectedFilepath = path.resolve(__dirname, 'fixtures', fixtureName, 'expected.' + extension);
+        var result = babel.transformFileSync(fixtureFilepath, {
             plugins: [
                 empowerAssert
             ]
-        }, extraOptions));
+        });
         var actual = result.code;
         var expected = fs.readFileSync(expectedFilepath).toString();
         assert.equal(actual + '\n', expected);
@@ -25,16 +24,16 @@ function testTransform (fixtureName, extraOptions, extraSuffix) {
 }
 
 describe('babel-plugin-empower-assert', function () {
-    testTransform('commonjs');
-    testTransform('commonjs_singlevar');
-    testTransform('commonjs_powerassert');
-    testTransform('commonjs_strictmode');
-    testTransform('commonjs_singlevar_strictmode');
-    testTransform('assignment');
-    testTransform('assignment_singlevar');
-    testTransform('assignment_strictmode');
-    testTransform('assignment_singlevar_strictmode');
-    testTransform('es6module');
-    testTransform('es6module_powerassert');
-    testTransform('es6module_namespece');
+    testTransform('commonjs', 'js');
+    testTransform('commonjs_singlevar', 'js');
+    testTransform('commonjs_powerassert', 'js');
+    testTransform('commonjs_strictmode', 'js');
+    testTransform('commonjs_singlevar_strictmode', 'js');
+    testTransform('assignment', 'js');
+    testTransform('assignment_singlevar', 'js');
+    testTransform('assignment_strictmode', 'js');
+    testTransform('assignment_singlevar_strictmode', 'js');
+    testTransform('es6module', 'mjs');
+    testTransform('es6module_powerassert', 'mjs');
+    testTransform('es6module_namespace', 'mjs');
 });
